5551c3527cdc515103c8b80a3c969e6a13e6f842
DuyLe2000 <56990897+DuyLe2000@users.noreply.github.com>
2021-09-10 16:57:02 +0700

    Format code

--- Objects/system_nuc100series.d
+++ Objects/system_nuc100series.d
@@ -1,29 +1,29 @@
 ./objects/system_nuc100series.o: \
-  ..\0_Library\NUC100Series\Source\system_NUC100Series.c \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\NUC100Series\Source\system_NUC100Series.c \
   C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  ..\0_Library\NUC100Series\Include\NUC100Series.h \
-  ..\0_Library\CMSIS\Include\core_cm0.h \
-  ..\0_Library\CMSIS\Include\core_cmInstr.h \
-  ..\0_Library\CMSIS\Include\core_cmFunc.h \
-  ..\0_Library\NUC100Series\Include\system_NUC100Series.h \
-  ..\0_Library\StdDriver\Include\SYS.h \
-  ..\0_Library\StdDriver\Include\ADC.h \
-  ..\0_Library\StdDriver\Include\FMC.h \
-  ..\0_Library\StdDriver\Include\GPIO.h \
-  ..\0_Library\StdDriver\Include\I2C.h \
-  ..\0_Library\StdDriver\Include\PWM.h \
-  ..\0_Library\StdDriver\Include\SPI.h \
-  ..\0_Library\StdDriver\Include\CRC.h \
-  ..\0_Library\StdDriver\Include\TIMER.h \
-  ..\0_Library\StdDriver\Include\WDT.h \
-  ..\0_Library\StdDriver\Include\WWDT.h \
-  ..\0_Library\StdDriver\Include\RTC.h \
-  ..\0_Library\StdDriver\Include\UART.h \
-  ..\0_Library\StdDriver\Include\I2S.h \
-  ..\0_Library\StdDriver\Include\CAN.h \
-  ..\0_Library\StdDriver\Include\USBD.h \
-  ..\0_Library\StdDriver\Include\PDMA.h \
-  ..\0_Library\StdDriver\Include\SC.h \
-  ..\0_Library\StdDriver\Include\PS2.h \
-  ..\0_Library\StdDriver\Include\CLK.h \
-  ..\0_Library\StdDriver\Include\ACMP.h
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\NUC100Series\Include\NUC100Series.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\CMSIS\Include\core_cm0.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\CMSIS\Include\core_cmInstr.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\CMSIS\Include\core_cmFunc.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\NUC100Series\Include\system_NUC100Series.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\SYS.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\ADC.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\FMC.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\GPIO.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\I2C.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\PWM.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\SPI.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\CRC.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\TIMER.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\WDT.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\WWDT.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\RTC.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\UART.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\I2S.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\CAN.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\USBD.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\PDMA.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\SC.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\PS2.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\CLK.h \
+  ..\..\..\2_Tutorial\ Keil\ Projects\0_Library\StdDriver\Include\ACMP.h
