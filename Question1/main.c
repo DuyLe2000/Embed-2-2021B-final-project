b96bffea35a856c8687be68fb2eb1f2a7c842e5f
dtnLong <49140143+dtnLong@users.noreply.github.com>
2021-09-14 23:06:27 +0700

    Add debounce

--- Question1/main.c
+++ Question1/main.c
@@ -11,12 +11,14 @@
 #define Ymax 64
 #define Xmin 1
 #define Ymin 1
-
+#define NO_INPUT -1;
+#define STAR -2;
 
 //------------------------------------------------------------------------------------------------------------------------------------
 // Functions declaration
 //------------------------------------------------------------------------------------------------------------------------------------
 void System_Config(void);
+void timer_Config(void);
 void SPI3_Config(void);
 void LCD_start(void);
 void LCD_command(unsigned char temp);
@@ -52,6 +54,15 @@ int main(void) {
     //--------------------------------
     System_Config();
     KeyPadEnable();
+    // Config timer for debounce
+    TIMER0->TCSR |= (1 << 26); // Reset timer
+    TIMER0->TCSR |= ~(3ul << 27); // One shot mode
+    TIMER0->TCSR &= ~(0xFF << 0); // Set prescale to 0
+    TIMER0->TCSR |= (1 << 16);
+
+    TIMER0->TCMPR &= ~(0xFFFFFFF << 0);
+    TIMER0->TCMPR |= 0x5B8D80; // 500 ms
+
     //--------------------------------
     //SPI3 initialization
     //--------------------------------
@@ -71,34 +82,41 @@ int main(void) {
 
     int8_t current_char = 0;
     char key_display[6] = "______";
+    int8_t key_delay[6] = {-1, -1, -1 , -1, -1, -1};
 
     while (1) {
         switch (lock_state) {
-            case option:
+            case option: {
                 // Option screen
+
+                // Reset all user input value
+                current_char = 0;
+                str_copy(key_display, "______");
+                for (int i = 0; i < 6; i++) {
+                    key_delay[i] = NO_INPUT;
+                }
+
                 printS_5x7(1, 1, "EEET2481 - Door Lock System");
                 printS_5x7(1, 10, "Please select");
                 printS_5x7(1, 19, "1: Unlock | 2: Change key");
-                while (key_press != 1 && key_press != 2) {
+                while (key_press != 1 && key_press != 2 && !(TIMER0->TCSR & (1 << 25))) {
+                    TIMER0->TCSR |= (1 << 30);
                     key_press = KeyPadScanning();
                 }
+
                 if (key_press == 1) {
-                    CLK_SysTickDelay(2000000);
                     // Clear the screen
                     LCD_clear();
                     lock_state = unlock;
                 } else if (key_press == 2) {
-                    CLK_SysTickDelay(2000000);
                     // Clear the screen
                     LCD_clear();
                     lock_state = change_key;
                 }
                 key_press = 0;
                 break;
-            case unlock: {
+            } case unlock: {
                 // Unlock enter key screen
-                int8_t key_delay[6] = {-1, -1, -1 , -1, -1, -1};
-                
                 printS_5x7(1, 1, "EEET2481 - Door Lock System");
                 printS_5x7(1, 9, "Please input your key");
                 printC_5x7(1, 17, key_display[0]);
@@ -113,38 +131,32 @@ int main(void) {
                     break;
                 }
 
-                // while (key_press == 0){
-                //     key_press = KeyPadScanning();
-                // }
-
-                key_press = KeyPadScanning();
+                if (!(TIMER0->TCSR & (1 << 25))) {
+                    TIMER0->TCSR |= (1 << 30);
+                    key_press = KeyPadScanning();
+                }
 
                 if (key_press != 0) {
-                    CLK_SysTickDelay(2000000);
                     key_display[current_char] = key_press + '0';
                     user_input[current_char] = key_press + '0';
                     current_char++;
                 }
-
+                key_press = 0;
+                
                 if (current_char == 6) {
                     if (str_compare(user_input, password)) {
                         printC_5x7(36, 17, key_display[5]);
+                        LCD_clear();
                         lock_state = welcome;
                     } else {
                         printC_5x7(36, 17, key_display[5]);
+                        LCD_clear();
                         lock_state = wrong_key;
                     }
                 }
-
-                key_press = 0;
-
                 break;
             } case change_key: {
                 // Change key enter screen
-                int8_t current_char = 0;
-                char key_display[6] = "______";
-                int8_t key_delay[6] = {-1, -1, -1 , -1, -1, -1};
-
                 printS_5x7(1, 1, "EEET2481 - Door Lock System");
                 printS_5x7(1, 9, "Please input new 6-digit key");
                 printC_5x7(1, 17, key_display[0]);
@@ -154,15 +166,27 @@ int main(void) {
                 printC_5x7(29, 17, key_display[4]);
                 printC_5x7(36, 17, key_display[5]);
 
-                key_press = KeyPadScanning();
+                if (!(TIMER0->TCSR & (1 << 25))) {
+                    TIMER0->TCSR |= (1 << 30);
+                    key_press = KeyPadScanning();
+                }
+
                 if (key_press != 0) {
                     key_display[current_char] = key_press + '0';
                     user_input[current_char] = key_press + '0';
                     current_char++;
                 }
+                key_press = 0;
 
-                if (current_char == 5) {
+                if (current_char == 6) {
                     str_copy(password, user_input);
+                    LCD_clear();
+                    printS_5x7(1, 1, "EEET2481 - Door Lock System");
+                    printS_5x7(1, 9, "Your key has been changed!");
+                    printS_5x7(1, 17, "THANK YOU!");
+                    CLK_SysTickDelay(800000);
+                    LCD_clear();
+                    lock_state = option;
                 }
 
                 if (!(PB->PIN & (1 << 15))) {
@@ -174,14 +198,19 @@ int main(void) {
                 printS_5x7(1, 1, "EEET2481 - Door Lock System");
                 printS_5x7(1, 9, "Welcome");
                 if (!(PB->PIN & (1 << 15))) {
+                    LCD_clear();
                     lock_state = option;
                 }
                 break;
             case wrong_key:
                 // Unlock failed screen
                 printS_5x7(1, 1, "EEET2481 - Door Lock System");
-                printS_5x7(1, 9, "Wrong key");
+                printS_5x7(1, 9, "The key is wrong");
+                printS_5x7(1, 17, "System restart in 1 second.");
+                printS_5x7(1, 15, "Thank you!");
+
                 if (!(PB->PIN & (1 << 15))) {
+                    LCD_clear();
                     lock_state = option;
                 }
                 break;
@@ -309,11 +338,13 @@ void System_Config(void) {
     //PLL configuration ends
     //clock source selection
     CLK->CLKSEL0 &= (~(0x07ul << 0));
-    CLK->CLKSEL0 |= (0x02ul << 0);
+    CLK->CLKSEL0 |= (0x02ul << 0); // 50 Mhz
     //clock frequency division
     CLK->CLKDIV &= (~0x0Ful << 0);
     //enable clock of SPI3
     CLK->APBCLK |= 1ul << 15;
+    // Set 12Mhz clock for timer 0
+    CLK->CLKSEL1 &= ~(0b111 << 8);
     SYS_LockReg(); // Lock protected registers
 }
 
